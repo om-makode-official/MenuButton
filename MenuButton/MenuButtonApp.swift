5bb89ae8e2dd4fc44ec03e6c12699322b1543664
Om Kashinath Makode <omkashinath_m@nexteducation.in>
2026-03-17 12:08:17 +0530

    Initial Commit

---
+++ MenuButton/MenuButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuButtonApp.swift
+//  MenuButton
+//
+//  Created by Sai Krishna on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
